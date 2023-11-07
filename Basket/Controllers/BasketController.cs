3b391809ec21357e6c30deb9885a340aefd2cdec
danibassetto <danibbassetto@hotmail.com>
2023-11-07 17:18:46 -0300

    new

---
+++ Basket/Controllers/BasketController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MsBasket.Entities;
+using MsBasket.Repositories;
+
+namespace MsBasket.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{userName}", Name ="GetBasket")]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+
+            return Ok(basket ?? new ShoppingCart(userName));  // se for null significa que é o primeiro acesso, então eu crio a cesta com o nome do usuário
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket) 
+        {
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{userName}", Name ="DeleteBasket")]
+        public async Task<ActionResult> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok();    
+        }
+    }
+}
---
+++ Basket/Repositories/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using MsBasket.Entities;
+
+namespace MsBasket.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
